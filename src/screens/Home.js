0b3f30da02d3901a7acd87114ef3d81d2cfb43d9
Vladislav <nykaloff1work@gmail.com>
2021-08-22 22:56:33 +0300

    cleanup code

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import { StyleSheet, Button, View, SafeAreaView, } from 'react-native';
+import React from "react";
+import { StyleSheet, Button, View, SafeAreaView } from "react-native";
 
-
-export const Home = ({ navigation }) => {
+export const Home = (props) => {
+  const navigation = props.navigation;
   return (
     <SafeAreaView style={styles.container}>
       <View>
         <Button
           title="Страница1"
-          onPress={() =>
-            navigation.navigate('Screen1')
-          }
+          onPress={() => navigation.navigate("Screen1")}
         />
       </View>
     </SafeAreaView>
-  )
+  );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    justifyContent: "center",
     marginHorizontal: 100,
   },
 });
--- src/screens/Screen1.js
+++ src/screens/Screen1.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React from "react";
+import { Text, View } from "react-native";
 
 export const Screen1 = () => {
-    return (
+  return (
     <View>
-    <Text>Скрин1</Text>
+      <Text>Скрин1</Text>
     </View>
-    )
+  );
 };
