de2ce1d25dfab6f076e2cb9c6bdbf89cde2d2dc9
Vladislav <nykaloff1work@gmail.com>
2021-09-01 01:12:41 +0300

    Text Input fix

--- src/components/Text-input.js
+++ src/components/Text-input.js
@@ -1,18 +1,22 @@
 import React from "react";
-import { SafeAreaView, StyleSheet, TextInput } from "react-native";
+import { SafeAreaView, StyleSheet, TextInput, Text } from "react-native";
+import { Resultinput } from "./Resultinput";
 
 export const InputPlace = () => {
-  const [number, onChangeNumber] = React.useState(null);
+  const [number, setNumber] = React.useState(null);
+  const onChangeText = (text) => {
+    setNumber(text);
+  };
 
   return (
     <SafeAreaView>
       <TextInput
         style={styles.input}
-        onChangeText={onChangeNumber}
         value={number}
         placeholder="Enter your number"
-        keyboardType="numeric"
+        onChangeText={onChangeText}
       />
+      <Resultinput value={number} />
     </SafeAreaView>
   );
 };
--- src/screens/Screen2.js
+++ src/screens/Screen2.js
@@ -1,4 +1,13 @@
 import React from "react";
 import { InputPlace } from "../components/Text-input";
+import { Resultinput } from "../components/Resultinput";
+import { View } from "react-native";
 
-export const Screen2 = () => <InputPlace />;
+export const Screen2 = () => {
+  return (
+    <View>
+      <InputPlace />
+      <Resultinput />
+    </View>
+  );
+};
