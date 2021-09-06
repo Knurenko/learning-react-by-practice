b1ea6de961cabf7f5bcb3e83864b0d1ee61348c6
Vladislav <nykaloff1work@gmail.com>
2021-09-06 15:20:41 +0300

    styles

--- src/components/ResultChecker.js
+++ src/components/ResultChecker.js
@@ -1,21 +1,34 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
 
 export const ResultChecker = (props) => {
   const isValueNumber = !isNaN(props.value);
   const isNumberOdd = (n) => n % 2 === 0;
+
   const resultText = () => {
     return isValueNumber
       ? isNumberOdd(props.value)
-        ? "Number is Odd"
-        : "Number is't Odd"
-      : "It is not a Number";
+        ? "Кртано 2"
+        : "Не кратно 2"
+      : "Это не число, введите число!";
   };
 
   const txt = resultText();
   return (
     <View>
-      <Text>{txt}</Text>
+      <Text style={styles.txt}>{txt}</Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  txt: {
+    textAlign: "center",
+    color: "green",
+    height: 30,
+    marginLeft: 80,
+    marginRight: 80,
+    borderWidth: 2,
+    padding: 5,
+  },
+});
--- src/screens/Screen1.js
+++ src/screens/Screen1.js
@@ -3,9 +3,9 @@ import { Text, View, Button, StyleSheet } from "react-native";
 
 export const Screen1 = () => {
   const [click, setClick] = React.useState(0);
-  const name1 = "Example 1";
+  const name1 = "Name 1";
   const [name, setName] = React.useState(name1);
-  const name2 = "Expamle 2";
+  const name2 = "Name 2";
   return (
     <View style={styles.container}>
       <Text>Click № {click}</Text>
