af1dfac564c4cfd78f2f232d35bb568ffe20e594
Vladislav <nykaloff1work@gmail.com>
2021-09-29 23:55:17 +0300

    ковырял react native debugger

--- src/redux/product/reducer.js
+++ src/redux/product/reducer.js
@@ -1,11 +1,7 @@
-import { productActionTypes } from "./action";
 import { productList } from "./productList";
 
 export const productReducer = (state = productList, action) => {
   switch (action.type) {
-    case productActionTypes.SET_PRODUCT:
-      return { ...state, product: action.payload };
-
     default:
       return state;
   }
